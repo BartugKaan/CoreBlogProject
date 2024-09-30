5a0b943cd5ef5ade93e5d757243931fc0ec5c82e
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-09-30 23:51:58 +0300

    add: Category Service and Category Manager

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class CategoryManager : ICategoryService
+    {
+
+        public void AddCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Category GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
